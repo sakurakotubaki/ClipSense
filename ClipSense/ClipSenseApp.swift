5dc54b7c02729a7466d2a845d7c2110e37e2539c
sakurakotubaki <gitonly543@gmail.com>
2026-04-26 22:26:31 +0900

    Initial Commit

---
+++ ClipSense/ClipSenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipSenseApp.swift
+//  ClipSense
+//
+//  Created by 橋本純一 on 2026/04/26.
+//
+
+import SwiftUI
+
+@main
+struct ClipSenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
